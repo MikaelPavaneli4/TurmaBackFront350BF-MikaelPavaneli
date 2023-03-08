b89b988f83db3b2fe4119fd7d8aaf5481ce372a8
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-03-07 22:54:40 -0300

    Fizemos exemplos e explicações de construtores e mais sobre classe

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -39,19 +39,92 @@ class Cachorro {
     var corDoPelo: String = "caramelo"
     var pesoDoCachorro: Double = 20.00
     var comida: String = "Ração"
-    var raça: String = "Doberman"
+    var raca: String = "Doberman"
     var quantidadeDePatas: Int = 4
     
-    func correr(){
+    func correr() {
         print("O cachorro está correndo")
     }
     
-    func latir(){
+    func latir() {
         print("O cachorro está latindo")
         
     }
 }
 
+
+
+class Videogame {
+    
+    var cor: String = "preto"
+    var peso: Double = 7.50
+    var preco: Float = 1800.50
+    var pilhas: Bool = true
+    var controle: Int = 2
+    
+    func ligar(){
+        print("O video Game está ligando.")
+        
+    }
+    func rodarJogo(){
+        print("rodando o jogo!!")
+    }
+    
+}
+
+
+// Construtores
+// Criamos um construtor com a palavra reservada init e com isso indicamos TODOS OS VALORES DAS VARIAVEIS OU CONSTANTES QUE NÃO SE INICIAM COM VALOR
+
+class Pessoa {
+    
+    var nome: String
+    var altura: Double
+    var peso: Double
+    var tamanhoCalcado: Int
+    
+    
+    init(nome: String, altura: Double, peso: Double, tamanhoCalcado: Int) {
+        self.nome = nome
+        self.altura = altura
+        self.peso = peso
+        self.tamanhoCalcado = tamanhoCalcado
+    }
+}
+
+var caio: Pessoa = Pessoa(nome: "Caio", altura: 1.84, peso: 84, tamanhoCalcado: 44)
+var matheus: Pessoa = Pessoa(nome: "Matheus", altura: 1.65, peso: 69, tamanhoCalcado: 40)
+//print(caio.nome)
+//print(caio.altura)
+//print(caio.peso)
+
+//  Referencia uma das principais caracteristicas de uma classe
+
+print(caio.nome)
+print(matheus.nome)
+
+caio.nome = "Caio Fabrini"
+matheus.nome = "Matheus Souza"
+
+print(caio.nome)
+print(matheus.nome)
+
+// caio é igual a matheus
+caio = matheus
+
+print(caio.nome)
+print(matheus.nome)
+
+
+caio.nome = "Alberto"
+
+print(caio.nome)
+print(matheus.nome)
+
+matheus.nome = "Jorge"
+
+print(caio.nome)
+print(matheus.nome)
 
 
 
