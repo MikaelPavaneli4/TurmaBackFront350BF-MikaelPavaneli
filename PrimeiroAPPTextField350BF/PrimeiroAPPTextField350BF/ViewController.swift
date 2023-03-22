76c2f6ef1d46a14116feff049f18212f811dfe21
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-03-21 21:48:29 -0300

    criei um novo arquivo para aprender mais sobre viewControler

---
+++ PrimeiroAPPTextField350BF/PrimeiroAPPTextField350BF/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PrimeiroAPPTextField350BF
+//
+//  Created by Mikael Rodrigues on 21/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        nameTextField.placeholder = "Digite seu nome:"
+        
+    }
+
+
+
+}
+
