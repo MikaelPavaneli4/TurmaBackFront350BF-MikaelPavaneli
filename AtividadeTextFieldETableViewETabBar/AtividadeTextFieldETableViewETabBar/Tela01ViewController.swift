6c81abafac6cfb40723f8e39e35ddf09c734d75e
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-05-05 13:41:07 -0300

    configurandos os elementos

--- AtividadeTextFieldETableViewETabBar/AtividadeTextFieldETableViewETabBar/Tela01ViewController.swift
+++ AtividadeTextFieldETableViewETabBar/AtividadeTextFieldETableViewETabBar/Tela01ViewController.swift
@@ -12,7 +12,7 @@ class Tela01ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 
--- AtividadeTextFieldETableViewETabBar/AtividadeTextFieldETableViewETabBar/Tela02ViewController.swift
+++ AtividadeTextFieldETableViewETabBar/AtividadeTextFieldETableViewETabBar/Tela02ViewController.swift
@@ -9,21 +9,52 @@ import UIKit
 
 class Tela02ViewController: UIViewController {
 
+    @IBOutlet weak var usuariosTableView: UITableView!
+    @IBOutlet weak var adicionarButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var fotoUsuarioImage: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+configElementos()
+        configTableView(TableView: usuariosTableView)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configElementos(){
+        nameLabel.text = "Nome:"
+        nameLabel.textColor = UIColor.gray
+        nameTextField.delegate = self
+        nameTextField.placeholder = "Coloque o nome de usuario"
+        nameTextField.layer.masksToBounds = true
+        nameTextField.layer.cornerRadius = 2
+        nameTextField.layer.borderWidth = 2
+        adicionarButton.
+        adicionarButton.layer.cornerRadius = 3
+        adicionarButton.layer.borderColor = UIColor.blue.cgColor
+        adicionarButton.layer.borderWidth = 3
     }
-    */
+    
+    func configTableView(TableView: UITableView){
+        TableView.dataSource = self
+        TableView.delegate = self
+    }
 
 }
+
+extension Tela02ViewController: UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        <#code#>
+//    }
+    
+}
+
