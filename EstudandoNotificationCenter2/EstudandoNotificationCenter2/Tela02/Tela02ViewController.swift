e5e7732e78163f37d5b997f6b550dc26b6fa072e
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-05-15 14:30:58 -0300

    criando o projetos, os elementos e fazendo ligacoes

---
+++ EstudandoNotificationCenter2/EstudandoNotificationCenter2/Tela02/Tela02ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Tela02ViewController.swift
+//  EstudandoNotificationCenter2
+//
+//  Created by Mikael Rodrigues on 15/05/23.
+//
+
+import UIKit
+
+class Tela02ViewController: UIViewController {
+
+    @IBOutlet weak var voltarButton: UIButton!
+    @IBOutlet weak var nomeTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func tappedVoltarButton(_ sender: UIButton) {
+    }
+    
+    
+
+}
---
+++ EstudandoNotificationCenter2/EstudandoNotificationCenter2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  EstudandoNotificationCenter2
+//
+//  Created by Mikael Rodrigues on 15/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var avancarButton: UIButton!
+    @IBOutlet weak var nomeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+    @IBAction func tappedAvancarButton(_ sender: UIButton) {
+    }
+    
+}
+
